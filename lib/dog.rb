860b4c4eb3e6f915f75128dbd4bf4095e07a9507
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-21 04:21:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,15 @@
 # Your code goes here!
 class Dog
+ def name= (dog_name)
+   @this_dogs_name = dog_name
+ end
 
+ def name
+   @this_dogs_name
+ end
 
 end
+
+fido = Dog.new
+fido.name = "fido"
+puts fido.name
